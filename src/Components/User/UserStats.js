9bbaa78f6a15a628e78ced7a79eca1858dc42a5a
Jefferson Guirra <jeffersonloop14@gmail.com>
2022-08-05 23:25:00 -0300

    Update UserStats.js

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -4,7 +4,8 @@ import useFetch from '../../Hooks/useFetch'
 import { STATS_GET } from '../../api'
 import Loading from '../Helper/Loading'
 import Error from '../Helper/Error'
-const UserStatsGraphs = React.lazy(()=>import('./UserStatsGraphs'))
+import UserStatsGraphs from './UserStatsGraphs'
+/*const UserStatsGraphs = React.lazy(()=>import('./UserStatsGraphs'))*/
 
 const UserStats = ()=>{
   const {data,error,loading,request} = useFetch()
@@ -20,10 +21,12 @@ const UserStats = ()=>{
   if(error) return <Error error={error} />
   if(data)
   return(
-    <React.Suspense fallback={<div></div>}>
-      <Head title='Estatísticas'/>
-      <UserStatsGraphs data={data}/>
-    </React.Suspense>
+      <>
+        <Head title='Estatísticas'/>
+        <UserStatsGraphs data={data} />
+      </>
+
+    
   )
   else return null
 }
